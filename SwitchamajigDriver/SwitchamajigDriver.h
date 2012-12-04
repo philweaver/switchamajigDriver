576c8cd21eb6b839126a7988cf24deb8f23dce56
Phil Weaver <philweaver@emu.(none)>
2012-12-04 14:04:11 -0800

    Adding documentation for the Switchamajig Driver.

--- SwitchamajigDriver/SwitchamajigDriver.h
+++ SwitchamajigDriver/SwitchamajigDriver.h
@@ -17,6 +17,52 @@ const NSString *SwitchamajigDriverErrorDomain;
 #define SJDriverErrorIR 2000
 #define SJDriverErrorIRDatabase 2001
 
+/*------------------------------------------------------------------------------------------------------------
+ Switchamajig Driver
+  
+ The driver consist of two parts: the driver and the listener. The listener listens for Switchamajig devices
+ on the network, and the driver talks to them.
+ 
+ Both the driver and the listener use delegates. The listener calls its delegate when it finds a device to
+ talk to, and the driver calls its delegate with connection status information.
+ 
+ The driver also allows commands to be sent to the Switchamajig device. These are specified as XML.
+ 
+ The XML commands supported depend on the type of device:
+ Switchamajig Controller - Basic Operation
+ turnSwitchesOn - Closes selected relays
+    <turnSwitchesOn>1 4 5</turnSwitchesOn> - Closes relays for ports 1, 4, and 5. Ports are numbered 1-6.
+ turnSwitchesOff - Opens selected relays
+    <turnSwitchesOff>2</turnSwitchesOff> - Opens relay for port 2.
+
+ Switchamajig Controller - Special Commands
+ setDeviceName - Set the friendly name for the device
+    <setDeviceName>California</setDeviceName> - Sets the device's friendly name to 'California'
+ configureDeviceNetworking - Change the networking settings. Note that the Switchamajig Controller reboots
+    after you change its network settings. It then tries to join the new network. If that network is different
+    from the iPad's, the iPad will lose contact with it.
+    <configureDeviceNetworking ssid="newSSID" channel="3" passphrase="newPassprase"></configureDeviceNetworking>
+ 
+ Switchamajig IR - Basic Operation
+ docommand - Send one or more IR commands
+    <docommand key="0" repeat="1" seq="0" command="AnyString" ir_data="irCommands" ch="0"></docommand>
+        key: Reserved for future use. Set to 0.
+        repeat: The number of times to send out the command sequence.
+        seq: Reserved for future use. Set to 0.
+        ir_data: A string from the IR database or from learning an IR command. Commands may be concatenated 
+                 together and separated by a space.
+        ch: Reserved for future use. Set to 0.
+ 
+ Note that there is no way to configure the networking settings of the Switchamajig IR. Its networking is 
+ configured over USB using a program available on switchamjig.com.
+ 
+ The Switchamjig IR driver also supports commands to interrogate the database of IR codes and to learn a new code.
+ Learning is started with - (void) startIRLearning; the response is returned to the delegate, which must
+ implement additional methods to be a SwitchamajigIRDeviceDriverDelegate.
+ 
+ The IR database returns arrays of strings for the brands, devices, and code sets for the IR codes. It returns
+ a string when provided with a valid brand, device, and code set.
+ ------------------------------------------------------------------------------------------------------------*/
 @protocol SwitchamajigDeviceDriverDelegate <NSObject>
 @required
 - (void) SwitchamajigDeviceDriverConnected:(id)deviceDriver;
